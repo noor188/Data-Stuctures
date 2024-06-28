3302ccc146624616503b686fc6028c60e1247fb3
Noor <noormmlk@gmail.com>
2024-06-28 15:13:08 -0600

    wish list entries for Nlist class

--- list.py
+++ list.py
@@ -1,4 +1,62 @@
 
+
 class Nlist:
     '''represents numbers within a certain range'''
-    pass
+    
+    def __init__(self) -> None:
+        self.items = []
+
+    def append(self, item) -> None:
+        '''
+        Adds an item to the list.
+
+        Parameters: 
+        item: value (int, float, str, boolean, object ... etc)
+
+        Return:
+        None
+        ''' #TODO
+        
+    def remove(self, item) -> None:
+        '''
+        removes item from the list
+
+        Parameters:
+        item: value (int, float, str, boolean, object ... etc)
+
+        Return:
+        None
+        ''' #TODO  
+
+    def get(self, index):
+        '''
+        returns the item at the specified index
+
+        Parameters:
+        index (int) : the index of item in the list
+
+        Return:
+        item: value (int, float, str, boolean, object ... etc)
+        ''' #TODO     
+
+    def size(self) -> int:
+        '''
+        returns the number of items in the list
+
+        Parameters:
+        None
+
+        Return:
+        size (int) : the size of the list
+        '''  #TODO
+
+    def is_empty(self) -> bool:
+        '''
+        return true if list has zero items
+
+        Parameters:
+        None
+
+        Return:
+        bool : true if number of items = 0
+        ''' #TODO
